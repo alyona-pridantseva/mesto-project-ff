e88e6c3c1af9962ce16911b1328ab574c9fc058e
Alyona Pridantseva <pridantseva.alyona@gmail.com>
2024-04-16 03:16:49 +0300

    первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,43 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content; //выбираю эл-т по идентификатору, получаю доступ к контенту
 
 // @todo: DOM узлы
-
-// @todo: Функция создания карточки
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция удаления карточки
+function cardDelete(event) {
+  const cardItem = event.target.closest(".card");
+  cardItem.remove();
+}
+
+// @todo: Функция создания карточки
+function createCard(cardData, cardDelete) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true); //клонирую шаблон
+  cardElement.querySelector(".card__image").src = cardData.link; //установка значений вложенных эл-в
+  cardElement.querySelector(".card__image").alt = cardData.name; //установка значений вложенных эл-в
+  cardElement.querySelector(".card__title").textContent = cardData.name; //установка значений вложенных эл-в
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  deleteButton.addEventListener("click", cardDelete); // function (event) { cardDelete(event); }
+
+  return cardElement;
+}
 
 // @todo: Вывести карточки на страницу
+function placeContentForLoop() {
+  for (let i = 0; i < initialCards.length; i++) {
+    const cardElement = createCard(initialCards[i], cardDelete);
+    placesList.append(cardElement); // возможно, стоит добавить проверку на то, что элемент уже добавлен в placeList, для избежания дубликатов
+  }
+}
+
+// function placeContentForEach() {
+//   initialCards.forEach(function (cardData) {
+//     const cardElement = createCard(cardData, cardDelete);
+//     placesList.append(cardElement);
+//   });
+// }
+
+placeContentForLoop();
+// placeContentForEach();
